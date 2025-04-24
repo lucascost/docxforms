d8ef95206580a17d9a3e7a2b98223ff9a7723a2a
lucascost <lucascost.dev@gmail.com>
2025-04-24 10:30:02 -0300

    adiciona upload de arquivo .docx via formulario

--- docxforms/core/views.py
+++ docxforms/core/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from docxtpl import DocxTemplate
 
 def inicio(request):
+    if request.method == 'POST':
+        doc = DocxTemplate(request.FILES['docx_file'])
+        print("arquivo recebido")
+
+
     return render(request, 'index.html')

6c9a8c54c2de019928d674a3f3f44017d20d16c5
lucas costa <lucascost.dev@gmail.com>
2025-04-25 14:59:00 -0300

    adiciona tratamento de erros e separação de funções

--- docxforms/core/views.py
+++ docxforms/core/views.py
@@ -1,7 +1,5 @@
 import base64
-from io import BytesIO
 import re
-import zipfile
 from django import forms
 from django.shortcuts import redirect, render
 from docxtpl import DocxTemplate
@@ -24,36 +22,40 @@ def extrair_campos(docx):
                             campos.append(match)
     return campos
 
-def inicio(request):
+def serializar_arquivo(arquivo):
+    bytes = arquivo.read()
+    return base64.b64encode(bytes).decode('utf-8')
 
+def guardar_em_memoria(request, key, value):
+    request.session[key] = value
+
+
+def inicio(request):
     if request.method == 'POST':
         try:
-            docx = DocxTemplate(request.FILES['docx_file'])
+            file = request.FILES['docx_file']
 
-            if not request.FILES['docx_file'].name.endswith('.docx'):
+            if not file.name.endswith('.docx'):
                 return render(request, 'index.html', {'error': 'O arquivo enviado não é um DOCX válido'})
 
+            docx = DocxTemplate(file)
             campos = extrair_campos(docx)
             
+            docx_bytes = serializar_arquivo(file)
+            guardar_em_memoria(request, 'docx_bytes', docx_bytes)
+            guardar_em_memoria(request, 'campos', campos)
+
+            return redirect('edit/')
 
         except KeyError:
             return render(request, 'index.html', {'error': 'Arquivo DOCX não enviado'})
         except Exception as e:
             return render(request, 'index.html', {'error': 'Erro ao processar o arquivo'})
-        
-        docx = request.FILES['docx_file']
-        docx_bytes= docx.read()
-        
-        docx_serializado = docx_bytes.decode('latin1')
-
-        request.session['docx_bytes'] = docx_serializado
-        request.session['campos'] = campos
-        
-        return redirect('edit/')
 
     return render(request, 'index.html')
 
 def editar_docx(request):
     campos_recebidos = request.session.get('campos',[])
     campos_formatados = [s.replace("_", " ") for s in campos_recebidos]
+    
     return render(request, 'edit.html', {'campos':campos_formatados})

3cce6573b8b5f27431cb2574c7d8bf1bf9495f09
lucascost <lucascost.dev@gmail.com>
2025-04-25 11:02:52 -0300

    Adiciona geração de formulário dinâmico com base nas chaves extraídas

--- docxforms/core/urls.py
+++ docxforms/core/urls.py
@@ -4,5 +4,6 @@ from core import views
 app_name='core'
 
 urlpatterns = [
-    path('', views.inicio, name='inicio')
+    path('', views.inicio, name='inicio'),
+    path('edit/', views.editar_docx, name='editar')
 ]
--- docxforms/core/views.py
+++ docxforms/core/views.py
@@ -1,6 +1,9 @@
+import base64
+from io import BytesIO
 import re
 import zipfile
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import redirect, render
 from docxtpl import DocxTemplate
 
 def extrair_campos(docx):
@@ -31,15 +34,26 @@ def inicio(request):
                 return render(request, 'index.html', {'error': 'O arquivo enviado não é um DOCX válido'})
 
             campos = extrair_campos(docx)
-            print(campos)
+            
 
         except KeyError:
             return render(request, 'index.html', {'error': 'Arquivo DOCX não enviado'})
         except Exception as e:
             return render(request, 'index.html', {'error': 'Erro ao processar o arquivo'})
-            
+        
+        docx = request.FILES['docx_file']
+        docx_bytes= docx.read()
+        
+        docx_serializado = docx_bytes.decode('latin1')
+
+        request.session['docx_bytes'] = docx_serializado
+        request.session['campos'] = campos
+        
+        return redirect('edit/')
 
     return render(request, 'index.html')
 
-def editar_docx(request, campos, docx):
-    pass
+def editar_docx(request):
+    campos_recebidos = request.session.get('campos',[])
+    campos_formatados = [s.replace("_", " ") for s in campos_recebidos]
+    return render(request, 'edit.html', {'campos':campos_formatados})
